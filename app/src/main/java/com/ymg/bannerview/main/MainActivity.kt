d3a8ae1396ed776e17c8780e4267e21932b7af4c
y-mg <ymg926@naver.com>
2020-11-25 10:09:16 +0900

    Update ViewBinding

--- app/src/main/java/com/ymg/bannerview/main/MainActivity.kt
+++ app/src/main/java/com/ymg/bannerview/main/MainActivity.kt
@@ -2,17 +2,23 @@ package com.ymg.bannerview.main
 
 import android.os.Bundle
 import com.ymg.bannermodule.auto.AutoBannerViewPager.Companion.AUTO_SCROLL_DIRECTION_RIGHT
-import com.ymg.bannerview.R
-import kotlinx.android.synthetic.main.activity_main.*
+import com.ymg.bannerview.databinding.ActivityMainBinding
+
 
 class MainActivity : BasicActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
         val mainAdapter = MainAdapter(this)
 
-        autoBannerViewPager.apply {
+        viewBinding.autoBannerViewPager.apply {
             adapter = mainAdapter
             setShowBannerDuration(1000) // 배너를 보여줄 지속 시간
             setAutoScrollDirection(AUTO_SCROLL_DIRECTION_RIGHT) // 자동 스크롤 방향
@@ -23,7 +29,9 @@ class MainActivity : BasicActivity() {
             setStartAutoScroll(5000) // 자동 스크롤 시작 및 첫 배너 보여줄 시간 설정
         }
 
-        autoBannerIndicator.setConnectViewPager(autoBannerViewPager, 3) // 뷰페이저에 연결
-        autoBannerIndicator.setStartIndicator() // 인디케이터 시작
+        viewBinding.autoBannerIndicator.apply {
+            setConnectViewPager(viewBinding.autoBannerViewPager, 3) // 뷰페이저에 연결
+            setStartIndicator() // 인디케이터 시작
+        }
     }
 }
